bfc25f42316c89b6d5c2f0b0886d90e11126fb51
amoa400 <amoa400@163.com>
2014-08-20 18:09:55 +0800

    add rules

--- test.js
+++ test.js
@@ -1,5 +1,24 @@
-var fase = require('./lib/index.js')({
-	lang: 'cn'
-});
+"use strict";
 
-console.log(fase);
+var fase = require('./lib/index.js')();
+
+// validator
+var validator = fase.validator;
+
+validator.createModel('user', [
+	['name', 'length', [1, 3]],
+	['pass', 'length', [6, -1]],
+	['real', 'length', [-1, 9]],
+	['vvvv', 'amoa400']
+]);
+
+console.log(
+	validator.validate('user', {name: '1234567', vvvv: 1})
+);
+
+
+
+
+
+
+
